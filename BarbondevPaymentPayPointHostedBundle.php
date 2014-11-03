97225dd3529c81fd8301657acca8a224dd1788cc
Ashley <a.dawson@ashleydawson.co.uk>
2014-11-03 15:13:46 +0000

    Added bundle header class

---
+++ BarbondevPaymentPayPointHostedBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Barbondev\Payment\PayPointHostedBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class BarbondevPaymentPayPointHostedBundle
+ *
+ * @package Barbondev\Payment\PayPointHostedBundle
+ * @author Ashley Dawson <ashley.dawson@barbon.com>
+ */
+class BarbondevPaymentPayPointHostedBundle extends Bundle
+{
+
+}
